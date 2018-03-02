d44ff63e27352dcbe1cd43374a925372177df7ce
Azat Abdullin <azat.aam@gmail.com>
2018-03-02 18:49:07 +0300

    removed old files

--- src/main/kotlin/org/jetbrains/research/kfg/builder/MethodBuilder.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/builder/MethodBuilder.kt
@@ -10,6 +10,58 @@ import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.*
 import java.util.*
 
+class LocalArray : User<Value>, MutableMap<Int, Value> {
+    private val locals = mutableMapOf<Int, Value>()
+
+    override val entries: MutableSet<MutableMap.MutableEntry<Int, Value>>
+        get() = locals.entries
+    override val keys: MutableSet<Int>
+        get() = locals.keys
+    override val size: Int
+        get() = locals.size
+    override val values: MutableCollection<Value>
+        get() = locals.values
+
+    override fun clear() {
+        values.forEach { it.removeUser(this) }
+        locals.clear()
+    }
+
+    override fun put(key: Int, value: Value): Value? {
+        value.addUser(this)
+        val prev = locals.put(key, value)
+        if (prev != null) prev.removeUser(this)
+        return prev
+    }
+
+    override fun putAll(from: Map<out Int, Value>) {
+        from.forEach {
+            put(it.key, it.value)
+        }
+    }
+
+    override fun remove(key: Int): Value? {
+        val res = locals.remove(key)
+        if (res != null) res.removeUser(this)
+        return res
+    }
+
+    override fun containsKey(key: Int) = locals.containsKey(key)
+    override fun containsValue(value: Value) = locals.containsValue(value)
+    override fun get(key: Int) = locals.get(key)
+    override fun isEmpty() = locals.isEmpty()
+
+    override fun replaceUsesOf(from: Value, to: Value) {
+        entries.forEach { (key, value) ->
+            if (value == from) {
+                value.removeUser(this)
+                locals[key] = to
+                to.addUser(this)
+            }
+        }
+    }
+}
+
 class MethodBuilder(val method: Method, val mn: MethodNode)
     : JSRInlinerAdapter(Opcodes.ASM5, mn, mn.access, mn.name, mn.desc, mn.signature, mn.exceptions.map { it as String }.toTypedArray()) {
     val ST = method.slottracker
