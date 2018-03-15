5f37755b70602a8296bbeb00b64d75363eeda224
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-03-15 13:05:01 +0300

    m

--- src/main/kotlin/org/jetbrains/research/kfg/ir/Class.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/ir/Class.kt
@@ -36,6 +36,9 @@ abstract class Class(val cn: ClassNode) : Node(cn.name.substringAfterLast('/'), 
     fun getOuterMethod() = if (cn.outerMethod != null) getOuterClass()?.getMethod(cn.outerMethod, cn.outerMethodDesc) else null
     fun getInnerClasses() = if (cn.innerClasses != null) cn.innerClasses.map { CM.getByName(it as String) } else listOf()
 
+    abstract fun getFieldConcrete(name: String, type: Type): Field?
+    abstract fun getMethodConcrete(name: String, desc: String): Method?
+
     abstract fun getField(name: String, type: Type): Field
     abstract fun getMethod(name: String, desc: String): Method
 
@@ -50,17 +53,29 @@ abstract class Class(val cn: ClassNode) : Node(cn.name.substringAfterLast('/'), 
 }
 
 class ConcreteClass(cn: ClassNode) : Class(cn) {
+    override fun getFieldConcrete(name: String, type: Type): Field? = fields.getOrElse(name, { getSuperClass()?.getFieldConcrete(name, type) })
+
+    override fun getMethodConcrete(name: String, desc: String): Method? = methods.getOrElse(MethodDesc(name, desc), {
+        val uppers = listOf(getSuperClass()).plus(getInterfaces()).filterNotNull()
+        val res: Method? = uppers
+                .mapNotNull { if (it is ConcreteClass) it else null }
+                .map { it.getMethodConcrete(name, desc) }
+                .firstOrNull() ?: uppers
+                .mapNotNull { if (it is OuterClass) it else null }
+                .firstOrNull()
+                ?.getMethodConcrete(name, desc)
+        res
+    })
+
     override fun getField(name: String, type: Type) = fields.getOrElse(name, {
-        getSuperClass()?.getField(name, type) ?: throw UnknownInstance("No field \"$name\" in class $this")
+        getSuperClass()?.getFieldConcrete(name, type) ?: throw UnknownInstance("No field \"$name\" in class $this")
     })
 
     override fun getMethod(name: String, desc: String): Method {
         val methodDesc = MethodDesc(name, desc)
         return methods.getOrElse(methodDesc, {
             val `super` = getSuperClass()
-            if (`super` != null && `super` is ConcreteClass && `super`.methods.containsKey(methodDesc))
-                `super`.methods[methodDesc]!!
-            else getInterfaces().filter { it is ConcreteClass }.filter { it.methods.containsKey(methodDesc) }.map { it.getMethod(name, desc) }.firstOrNull()
+            getMethodConcrete(name, desc)
                     ?: if (`super` != null && `super` is OuterClass)
                         `super`.getMethod(name, desc)
                     else getInterfaces().firstOrNull { it is OuterClass }?.getMethod(name, desc)
@@ -70,6 +85,9 @@ class ConcreteClass(cn: ClassNode) : Class(cn) {
 }
 
 class OuterClass(cn: ClassNode) : Class(cn) {
+    override fun getFieldConcrete(name: String, type: Type) = getField(name, type)
+    override fun getMethodConcrete(name: String, desc: String) = getMethod(name, desc)
+
     override fun getField(name: String, type: Type): Field = fields.getOrPut(name, {
         val fn = FieldNode(0, name, type.getAsmDesc(), null, null)
         Field(fn, this)
--- src/main/kotlin/org/jetbrains/research/kfg/ir/value/Constant.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/ir/value/Constant.kt
@@ -1,25 +1,114 @@
 package org.jetbrains.research.kfg.ir.value
 
 import org.jetbrains.research.kfg.TF
+import org.jetbrains.research.kfg.defaultHasCode
 import org.jetbrains.research.kfg.ir.Method
 import org.jetbrains.research.kfg.type.Type
 import org.jetbrains.research.kfg.type.parseDesc
 
 abstract class Constant(name: String, type: Type) : Value(name, type)
 
-class BoolConstant(val value: Boolean) : Constant(value.toString(), TF.getBoolType())
-class ByteConstant(val value: Byte) : Constant(value.toString(), TF.getByteType())
-class ShortConstant(val value: Short) : Constant(value.toString(), TF.getShortType())
-class IntConstant(val value: Int) : Constant(value.toString(), TF.getIntType())
-class LongConstant(val value: Long) : Constant(value.toString(), TF.getLongType())
-class CharConstant(val value: Char) : Constant(value.toString(), TF.getCharType())
-class FloatConstant(val value: Float) : Constant(value.toString(), TF.getFloatType())
-class DoubleConstant(val value: Double) : Constant(value.toString(), TF.getDoubleType())
+class BoolConstant(val value: Boolean) : Constant(value.toString(), TF.getBoolType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as BoolConstant
+        return this.value == other.value
+    }
+}
+
+class ByteConstant(val value: Byte) : Constant(value.toString(), TF.getByteType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as ByteConstant
+        return this.value == other.value
+    }
+}
+
+class ShortConstant(val value: Short) : Constant(value.toString(), TF.getShortType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as ShortConstant
+        return this.value == other.value
+    }
+}
+
+class IntConstant(val value: Int) : Constant(value.toString(), TF.getIntType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as IntConstant
+        return this.value == other.value
+    }
+}
+
+class LongConstant(val value: Long) : Constant(value.toString(), TF.getLongType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as LongConstant
+        return this.value == other.value
+    }
+}
+
+class CharConstant(val value: Char) : Constant(value.toString(), TF.getCharType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as CharConstant
+        return this.value == other.value
+    }
+}
+
+class FloatConstant(val value: Float) : Constant(value.toString(), TF.getFloatType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as FloatConstant
+        return this.value == other.value
+    }
+}
+
+class DoubleConstant(val value: Double) : Constant(value.toString(), TF.getDoubleType()) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as DoubleConstant
+        return this.value == other.value
+    }
+}
+
+class StringConstant(val value: String) : Constant("\"$value\"", TF.getRefType("java/lang/String")) {
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as StringConstant
+        return this.value == other.value
+    }
+}
+
+class MethodConstant(val method: Method) : Constant(method.name, TF.getRefType("java/lang/invoke/MethodHandle")) {
+    override fun hashCode() = method.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as MethodConstant
+        return this.method == other.method
+    }
+}
 
 class ClassConstant(`class`: Type) : Constant("${`class`.name}.class", `class`)
-class StringConstant(val value: String) : Constant("\"$value\"", TF.getRefType("java.lang.String"))
-class MethodConstant(val method: Method) : Constant(method.name, TF.getRefType("java.lang.invoke.MethodHandle"))
-
 class NullConstant : Constant("null", TF.getNullType()) {
     companion object {
         val instance = NullConstant()
