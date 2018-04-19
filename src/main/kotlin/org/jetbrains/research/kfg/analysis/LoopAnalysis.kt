7d55f3200780ced4091befc35f712c91f3ebd0e6
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-04-19 11:24:16 +0300

    loop simplifier

--- src/main/kotlin/org/jetbrains/research/kfg/analysis/LoopAnalysis.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/analysis/LoopAnalysis.kt
@@ -10,12 +10,14 @@ import org.jetbrains.research.kfg.util.GraphNode
 import org.jetbrains.research.kfg.util.LoopDetector
 import org.jetbrains.research.kfg.visitor.MethodVisitor
 
-class Loop(val header: BasicBlock, val body: Set<BasicBlock>) {
+class Loop(val header: BasicBlock, internal val body: MutableSet<BasicBlock>) {
     fun getExitingBlocks() = body.filter { !body.containsAll(it.successors) }.toSet()
     fun hasPreheader() = header.predecessors.filter { !body.contains(it) }.size == 1
     fun getPreheader() = header.predecessors.first { !body.contains(it) }
     fun getLatches() = body.filter { it.successors.contains(header) }.toSet()
     fun contains(bb: BasicBlock) = body.contains(bb)
+
+    fun addBlock(bb: BasicBlock) = body.add(bb)
 }
 
 class LoopAnalysis(method: Method) : MethodVisitor(method) {
@@ -23,47 +25,75 @@ class LoopAnalysis(method: Method) : MethodVisitor(method) {
     override fun visit() {
         val nodes = method.basicBlocks.map { it as GraphNode }.toSet()
         LoopDetector(nodes).search()
-                .map { Loop(it.key as BasicBlock, it.value.map { it as BasicBlock }.toSet()) }
+                .map { Loop(it.key as BasicBlock, it.value.map { it as BasicBlock }.toMutableSet()) }
                 .forEach { loops.add(it) }
     }
 }
 
-class LoopPreheaderBuilder(method: Method, val loops: List<Loop>) : MethodVisitor(method) {
+class LoopSimplifier(method: Method, val loops: List<Loop>) : MethodVisitor(method) {
     override fun visit() {
-        loops.filter { !it.hasPreheader() }.forEach { visitLoop(it) }
+        loops.forEach {
+            buildPreheader(it)
+            buildLatch(it)
+        }
+        IRVerifier(method).visit()
     }
 
-    private fun visitLoop(loop: Loop) {
-        val loopPredecessors = loop.header.predecessors.filter { !loop.contains(it) }
-        if (loopPredecessors.size == 1) return
+    private fun remapBlocks(target: BasicBlock, from: BasicBlock, to: BasicBlock) {
+        target.removeSuccessor(from)
+        from.removePredecessor(target)
+        target.addSuccessor(to)
+        from.addPredecessor(target)
+        (target.back() as TerminateInst).replaceUsesOf(from, to)
+    }
 
-        val preheader = BodyBlock("loop.preheader")
-        loopPredecessors.forEach {
-            it.removeSuccessor(loop.header)
-            loop.header.removePredecessor(it)
-            it.addSuccessor(preheader)
-            preheader.addPredecessor(it)
-            (it.back() as TerminateInst).replaceUsesOf(loop.header, preheader)
-        }
-        preheader.addSuccessor(loop.header)
-        loop.header.addPredecessor(preheader)
-        loop.header.instructions.mapNotNull { it as? PhiInst }.forEach {
-            val predKeys = it.getIncomings().filter { it.key in loopPredecessors }
-            val predValues = predKeys.values.toSet()
-            val preheaderVal = if (predValues.size == 1) predValues.first() else {
-                val phi = IF.getPhi(it.type, predKeys)
-                preheader.addInstruction(phi)
+    private fun remapPhis(target: BasicBlock, from: Set<BasicBlock>, to: BasicBlock) {
+        target.instructions.mapNotNull { it as? PhiInst }.forEach {
+            val fromIncomings = it.getIncomings().filter { it.key in from }
+            val fromValues = fromIncomings.values.toSet()
+            val toValue = if (fromValues.size == 1) fromValues.first() else {
+                val phi = IF.getPhi(it.type, fromIncomings)
+                to.addInstruction(phi)
                 phi
             }
 
-            val noPredKeys = it.getIncomings().filter { it.key !in loopPredecessors }.toMutableMap()
-            noPredKeys[preheader] = preheaderVal
-            val newHeaderPhi = IF.getPhi(it.type, noPredKeys)
-            loop.header.insertBefore(it, newHeaderPhi)
-            it.replaceAllUsesWith(newHeaderPhi)
-            loop.header.remove(it)
+            val targetIncomings = it.getIncomings().filter { it.key !in from }.toMutableMap()
+            targetIncomings[to] = toValue
+            val targetPhi = IF.getPhi(it.type, targetIncomings)
+            target.insertBefore(it, targetPhi)
+            it.replaceAllUsesWith(targetPhi)
+            target.remove(it)
         }
-        preheader.addInstruction(IF.getJump(loop.header))
-        method.addBefore(loop.header, preheader)
+    }
+
+    private fun buildPreheader(loop: Loop) {
+        val header = loop.header
+        val loopPredecessors = header.predecessors.filter { !loop.contains(it) }.toSet()
+        if (loopPredecessors.size == 1) return
+
+        val preheader = BodyBlock("loop.preheader")
+        loopPredecessors.forEach { remapBlocks(it, header, preheader) }
+        preheader.addSuccessor(header)
+        header.addPredecessor(preheader)
+
+        remapPhis(header, loopPredecessors, preheader)
+        preheader.addInstruction(IF.getJump(header))
+        method.addBefore(header, preheader)
+    }
+
+    private fun buildLatch(loop: Loop) {
+        val latches = loop.getLatches()
+        if (latches.size == 1) return
+
+        val header = loop.header
+        val latch = BodyBlock("loop.latch")
+        latches.forEach { remapBlocks(it, header, latch) }
+        latch.addSuccessor(header)
+        header.addPredecessor(latch)
+
+        remapPhis(header, latches, latch)
+        latch.addInstruction(IF.getJump(header))
+        method.add(latch)
+        loop.addBlock(latch)
     }
 }
