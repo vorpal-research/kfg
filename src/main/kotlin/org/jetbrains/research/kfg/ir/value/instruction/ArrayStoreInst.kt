1ba820b85d69c3ddf5de62204450ff042c0d1e67
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-09-05 11:04:05 +0300

    fixed: array store of boolean type converted to bastore

--- src/main/kotlin/org/jetbrains/research/kfg/ir/value/instruction/ArrayStoreInst.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/ir/value/instruction/ArrayStoreInst.kt
@@ -3,6 +3,7 @@ package org.jetbrains.research.kfg.ir.value.instruction
 import org.jetbrains.research.kfg.ir.value.UndefinedName
 import org.jetbrains.research.kfg.TF
 import org.jetbrains.research.kfg.ir.value.Value
+import org.jetbrains.research.kfg.type.ArrayType
 
 class ArrayStoreInst(arrayRef: Value, index: Value, value: Value)
     : Instruction(UndefinedName, TF.voidType, arrayOf(arrayRef, index, value)) {
@@ -15,6 +16,9 @@ class ArrayStoreInst(arrayRef: Value, index: Value, value: Value)
 
     val value: Value
         get() = ops[2]
+
+    val arrayComponent = (arrayRef.type as? ArrayType)?.component
+            ?: throw IllegalStateException("Non-array ref in array store")
 
     override fun print() = "$arrayRef[$index] = $value"
     override fun clone(): Instruction = ArrayStoreInst(arrayRef, index, value)
