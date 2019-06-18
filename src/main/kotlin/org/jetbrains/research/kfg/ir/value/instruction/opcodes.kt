ab172b2c5b55fe52437a680578afcf5250a48fe6
Azat Abdullin <azat.aam@gmail.com>
2019-06-18 20:35:47 +0300

    Predicate serialization using kotlinx.serialization

--- src/main/kotlin/org/jetbrains/research/kfg/ir/value/instruction/opcodes.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/ir/value/instruction/opcodes.kt
@@ -127,6 +127,18 @@ sealed class CmpOpcode {
     }
 
     override fun toString() = name
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CmpOpcode) return false
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
 
     class Eq(override val name: String = "==") : CmpOpcode()
     class Neq(override val name: String = "!=") : CmpOpcode()
