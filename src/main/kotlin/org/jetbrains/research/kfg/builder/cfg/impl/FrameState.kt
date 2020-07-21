7b59b8d459834edb52f189b08597fa1e2e463fdb
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-07-21 16:19:40 +0000

    release 0.0.9-5 fixes

--- src/main/kotlin/org/jetbrains/research/kfg/builder/cfg/impl/FrameState.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/builder/cfg/impl/FrameState.kt
@@ -23,6 +23,8 @@ private object TopType : Type {
     override val bitsize: Int
         get() = Type.WORD
 
+    override val isConcrete: Boolean
+        get() = true
     override fun isSubtypeOf(other: Type) = false
 }
 
@@ -36,6 +38,8 @@ private object UninitializedThisType : Type {
     override val bitsize: Int
         get() = Type.WORD
 
+    override val isConcrete: Boolean
+        get() = true
     override fun isSubtypeOf(other: Type) = false
 }
 
