6da0cf4f69da42f56b06ea3515beb4c2ef991121
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2021-06-07 18:58:29 +0300

    m

--- src/main/kotlin/org/jetbrains/research/kfg/Main.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/Main.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.research.kfg
 
+import org.jetbrains.research.kfg.analysis.LoopAnalysis
+import org.jetbrains.research.kfg.analysis.LoopSimplifier
 import org.jetbrains.research.kfg.container.asContainer
 import org.jetbrains.research.kfg.util.Flags
+import org.jetbrains.research.kfg.visitor.executePipeline
 import org.jetbrains.research.kthelper.logging.log
 import java.io.File
 
@@ -11,18 +14,16 @@ fun main(args: Array<String>) {
     val jar = File(cfg.getStringValue("jar")).asContainer()!!
 
     val classManager = ClassManager(KfgConfig(Flags.readAll, false))
-    log.error("aaaaaaaaaaaaaaa")
 
     classManager.initialize(jar)
-    log.error("aaaaaaaaaaaaaaa")
 
     log.debug(classManager.concreteClasses.joinToString("\n") { it.fullName })
-//    val target = File("instrumented/")
-//    println(jar.commonPackage)
-//    jar.unpack(classManager, target.toPath(), true, classManager.failOnError)
-//    executePipeline(classManager, jar.pkg) {
-//        +LoopAnalysis(classManager)
-//        +LoopSimplifier(classManager)
-//    }
-//    jar.update(classManager)
+    val target = File("instrumented/")
+    println(jar.commonPackage)
+    jar.unpack(classManager, target.toPath(), true, classManager.failOnError)
+    executePipeline(classManager, jar.pkg) {
+        +LoopAnalysis(classManager)
+        +LoopSimplifier(classManager)
+    }
+    jar.update(classManager)
 }
