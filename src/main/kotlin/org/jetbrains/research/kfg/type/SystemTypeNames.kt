3287ae8b7fd6ed2abe9c9cae657a8ba6c4f2342a
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2021-08-25 18:18:06 +0300

    more system type names

--- src/main/kotlin/org/jetbrains/research/kfg/type/SystemTypeNames.kt
+++ src/main/kotlin/org/jetbrains/research/kfg/type/SystemTypeNames.kt
@@ -20,11 +20,19 @@ object SystemTypeNames {
     const val arrayListClass = "java/util/ArrayList"
     const val linkedListClass = "java/util/LinkedList"
 
+    const val queueClass = "java/util/Queue"
+    const val dequeClass = "java/util/Deque"
+    const val arrayDequeClass = "java/util/ArrayDeque"
+
     const val setClass = "java/util/Set"
+    const val sortedSetClass = "java/util/SortedSet"
+    const val navigableSetClass = "java/util/NavigableSet"
     const val hashSetClass = "java/util/HashSet"
     const val treeSetClass = "java/util/TreeSet"
 
     const val mapClass = "java/util/Map"
+    const val sortedMapClass = "java/util/SortedMap"
+    const val navigableMapClass = "java/util/NavigableMap"
     const val hashMapClass = "java/util/HashMap"
     const val treeMapClass = "java/util/TreeMap"
 }
