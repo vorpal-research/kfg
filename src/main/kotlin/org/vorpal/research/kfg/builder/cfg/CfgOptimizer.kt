7937f9e301e34420529cba63ee83301e861e0e84
Azat Abdullin <azat.aam@gmail.com>
2023-01-28 14:44:31 +0300

    improve performance of CfgOptimizer

--- src/main/kotlin/org/vorpal/research/kfg/builder/cfg/CfgOptimizer.kt
+++ src/main/kotlin/org/vorpal/research/kfg/builder/cfg/CfgOptimizer.kt
@@ -1,6 +1,7 @@
 package org.vorpal.research.kfg.builder.cfg
 
 import org.vorpal.research.kfg.ClassManager
+import org.vorpal.research.kfg.ir.BasicBlock
 import org.vorpal.research.kfg.ir.CatchBlock
 import org.vorpal.research.kfg.ir.MethodBody
 import org.vorpal.research.kfg.ir.value.UsageContext
@@ -10,18 +11,26 @@ import org.vorpal.research.kfg.visitor.MethodVisitor
 class CfgOptimizer(override val cm: ClassManager, val ctx: UsageContext) : MethodVisitor {
     override fun cleanup() {}
 
-    override fun visitBody(body: MethodBody) = with (ctx) {
-        for (block in body.basicBlocks.toList()) {
-            if (block.isEmpty) continue
-            if (block.size > 1) continue
-            if (block.successors.size != 1) continue
-            if (block.predecessors.size != 1) continue
-            if (block.predecessors.first().successors.size != 1) continue
-            if (block.successors.first().predecessors.size != 1) continue
+    private val BasicBlock.canBeOptimized: Boolean
+        get() {
+            if (this.isEmpty) return false
+            if (this.size > 1) return false
+            if (this.successors.size != 1) return false
+            if (this.predecessors.size != 1) return false
+            if (this.predecessors.first().successors.size != 1) return false
+            if (this.successors.first().predecessors.size != 1) return false
 
+            val successor = this.successors.first()
+            if (this.handlers != successor.handlers) return false
+            return true
+        }
+
+    override fun visitBody(body: MethodBody) = with(ctx) {
+        val phiMappings = mutableMapOf<PhiInst, MutableMap<BasicBlock, BasicBlock?>>()
+        for (block in body.basicBlocks.toList()) {
+            if (!block.canBeOptimized) continue
             val successor = block.successors.first()
             val predecessor = block.predecessors.first()
-            if (block.handlers != successor.handlers) continue
 
             val handlers = block.handlers
             for (catch in handlers) {
@@ -32,25 +41,14 @@ class CfgOptimizer(override val cm: ClassManager, val ctx: UsageContext) : Metho
             for (phi in blockPhiUsers) {
                 val parent = phi.parentUnsafe ?: continue
 
-                val oldIncomings = phi.incomings
-                val incomings = when (parent) {
+                when (parent) {
                     is CatchBlock -> when (block) {
-                        in parent.entries -> oldIncomings.map {
-                            (if (it.key == block) predecessor else it.key) to it.value
-                        }.toMap()
-                        else -> oldIncomings.mapNotNull {
-                            if (it.key == block) null else it.key to it.value
-                        }.toMap()
+                        in parent.entries -> phiMappings.getOrPut(phi, ::mutableMapOf)[block] = predecessor
+                        else -> phiMappings.getOrPut(phi, ::mutableMapOf)[block] = null
                     }
-                    else -> oldIncomings.map {
-                        (if (it.key == block) predecessor else it.key) to it.value
-                    }.toMap()
-                }
-                val newPhi = inst(cm) { phi(phi.type, incomings) }
 
-                phi.replaceAllUsesWith(newPhi)
-                parent.replace(phi, newPhi)
-                phi.clearAllUses()
+                    else -> phiMappings.getOrPut(phi, ::mutableMapOf)[block] = predecessor
+                }
             }
 
             body.remove(block)
@@ -63,6 +61,22 @@ class CfgOptimizer(override val cm: ClassManager, val ctx: UsageContext) : Metho
             predecessor.addSuccessor(successor)
             successor.addPredecessor(predecessor)
             block.replaceAllUsesWith(successor)
+        }
+
+
+        for ((phi, mappings) in phiMappings) {
+            val parent = phi.parentUnsafe ?: continue
+            val incomings = phi.incomings.mapNotNull {
+                if (it.key in mappings) {
+                    if (mappings[it.key] == null) null
+                    else mappings[it.key]!! to it.value
+                } else it.key to it.value
+            }.toMap()
+            val newPhi = inst(cm) { phi(phi.type, incomings) }
+
+            phi.replaceAllUsesWith(newPhi)
+            parent.replace(phi, newPhi)
+            phi.clearAllUses()
         }
     }
 }
